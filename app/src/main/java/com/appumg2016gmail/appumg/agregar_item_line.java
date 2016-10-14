8eb3ed1e08ebea33f3ac3c50a31125155a49e87c
zombozo <gerson.olivares543@gmail.com>
2016-10-14 01:51:20 -0600

    enviado

--- app/src/main/java/com/appumg2016gmail/appumg/agregar_item_line.java
+++ app/src/main/java/com/appumg2016gmail/appumg/agregar_item_line.java
@@ -1,15 +1,32 @@
 package com.appumg2016gmail.appumg;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class agregar_item_line extends AppCompatActivity {
 private Button agregar;
+    private String direccion_imagen;
+
+    private String APP_DIRECTORY="MyPictureApp/";
+    private String MEDIA_DIRECTORY=APP_DIRECTORY+"PictureApp";
+
+    private final int MI_PERMISSIONS=100;
+    private final int PHOTO_CODE=200;
+    private final int SELECT_PICTURE=300;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +39,68 @@ private Button agregar;
 
 
     public void agregar(View view){
+        CharSequence[] opciones={"de la galeria","tomar foto", "cancelar"};
+        final AlertDialog.Builder builder=new AlertDialog.Builder(agregar_item_line.this);
+        builder.setTitle("Elige la opcion");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+              switch (which){
+                  case 0:
+                        tomar_foto();
+                      break;
+                  case 1:
+                      Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                      intent.setType("images/*");
+                      startActivityForResult(Intent.createChooser(intent,"seleccionar la imagen"),SELECT_PICTURE);
+                      break;
+                  case 2:
+                        dialog.dismiss();
+                      break;
+                  default:
 
+              }
+                builder.show();
+            }
+
+            private void tomar_foto() {
+                File file=new File(Environment.getExternalStorageDirectory(),MEDIA_DIRECTORY);
+                boolean isDirectoryCreated=file.exists();
+                if (isDirectoryCreated){
+                        isDirectoryCreated=file.mkdirs();
+                }else{
+                    Long timestam= System.currentTimeMillis()/1000;
+                    String ImageName=timestam.toString()+".jpg";
+                    direccion_imagen=Environment.getExternalStorageDirectory()+File.separator+MEDIA_DIRECTORY+file.separator+ImageName;
+                    File files=new File(direccion_imagen);
+                    Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(files));
+                    startActivityForResult(intent,PHOTO_CODE);
+
+                }
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+                switch (requestCode){
+                    case PHOTO_CODE:
+                        MediaScannerConnection.scanFile(this, new String[]{direccion_imagen}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("",""+path);
+                            }
+                        });
+                    case SELECT_PICTURE:
+                        Uri direccion=data.getData();
+                        break;
+                }
+        }
     }
 }
--- app/src/main/java/com/appumg2016gmail/appumg/time_line.java
+++ app/src/main/java/com/appumg2016gmail/appumg/time_line.java
@@ -2,10 +2,7 @@ package com.appumg2016gmail.appumg;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -20,10 +17,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class time_line extends AppCompatActivity
@@ -36,7 +29,7 @@ public class time_line extends AppCompatActivity
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
+    //
 
 
     @Override
@@ -70,7 +63,7 @@ public class time_line extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     @Override
@@ -143,36 +136,4 @@ public class time_line extends AppCompatActivity
         Toast.makeText(this, "has creado una base de datos", Toast.LENGTH_SHORT).show();
     }
 
-   
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("time_line Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
