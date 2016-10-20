f62fdc7924f00144894d43262ee2306bac80ec9c
appumg <appumg2016@gmail.com>
2016-10-19 22:14:35 -0600

    19/10/16  con 22:05

--- app/src/main/java/com/appumg2016gmail/appumg/time_line.java
+++ app/src/main/java/com/appumg2016gmail/appumg/time_line.java
@@ -141,7 +141,7 @@ public class time_line extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.menu_sobreUMG) {
 
         } else if (id == R.id.nav_share) {
 
