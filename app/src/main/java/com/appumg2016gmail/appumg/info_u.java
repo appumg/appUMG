7cac0ff71c1c2794fe9b0be34da2e674324afcf1
appumg <appumg2016@gmail.com>
2016-10-20 18:06:53 -0600

    19/10/16  con 22:05

--- app/src/main/java/com/appumg2016gmail/appumg/info_u.java
+++ app/src/main/java/com/appumg2016gmail/appumg/info_u.java
@@ -1,13 +1,44 @@
 package com.appumg2016gmail.appumg;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class info_u extends AppCompatActivity {
+
+
+    Toolbar toolbar;
+    android.support.design.widget.TabLayout TabLayout;
+    ViewPager viewPager;
+    adaptador_viewPager viewPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTheme(R.style.Tematab);/// Ponemos un tema especial para este activity, evita el duplicado de la barra de titulo
         setContentView(R.layout.activity_info_u);
+
+
+        toolbar=(Toolbar)findViewById(R.id.toolBar);//Creamos una instancia de la barrita que contendra un enlace a los fragment y sera los encabezados
+        setSupportActionBar(toolbar);///supongo que le enviamos el soporte para que se comporte como una actionBar
+
+        TabLayout= (android.support.design.widget.TabLayout) findViewById(R.id.tabLayout);//tabLayout es el layout que contendra los elementos
+        viewPager=(ViewPager)findViewById(R.id.viewPager);//visor de paginas
+
+        viewPagerAdapter=new adaptador_viewPager(getSupportFragmentManager());//Instacia del adaptador de los fragments
+        viewPagerAdapter.addFramnets(new historia_u(),"Historia UMG");//Agregamos framgens al adaptador
+        viewPagerAdapter.addFramnets(new himno_u(),"Himno");
+        viewPagerAdapter.addFramnets(new centros_u(),"Centros UMG");
+
+        viewPager.setAdapter(viewPagerAdapter);//enviamos el adaptadora al visor de paginas
+        TabLayout.setupWithViewPager(viewPager);//enviamos el visor de paginas al TabLayout
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/appumg2016gmail/appumg/time_line.java
+++ app/src/main/java/com/appumg2016gmail/appumg/time_line.java
@@ -1,12 +1,13 @@
 package com.appumg2016gmail.appumg;
 
-import android.app.Fragment;
-
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,79 +17,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Gallery;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
+
 public class time_line extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ListView TimeLine;
+    private String[] contenido = new String[]{"contendio", "conteido"};
+    private ArrayAdapter<String> adaptador;
+    private AdaptadorContentTimeLine adaptadorContentTimeLine;
+    private LinearLayout beahavior;
+    private GridView galeria;
+    private ArrayList<Integer> id_items=new ArrayList<>();
+
     private ArrayList<Uri> imagenes=new ArrayList<>();
-
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private NavigationView menu;
-    private int id=2131493035;
-
 ///----- creamos objetos de la base de datos y de la clase que crea la base de datos
+    private SQLiteDatabase itemsConenedor;
+    private db_itemsTimeLine db_itemsTimeLine;
+    private int version=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_line);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        galeria=(GridView) findViewById(R.id.galeria);
+
+/// ---- cargando las bases de datos
+
+
+
+
+        galeria.setAdapter(new galeria_adaptador(this,imagenes));
+
+
+        beahavior=(LinearLayout) findViewById(R.id.behavior);
+        final BottomSheetBehavior bottomSheetBehavior=BottomSheetBehavior.from(beahavior);
+
+       // ArrayList<Uri> imagenes=new ArrayList<Uri>(){};
+
+        // asignnando el adaptador al listview principal
+        TimeLine = (ListView) findViewById(R.id.Ttimeline);
+        //---- llamamos el metodo que cargar la linea de tiempo
+        cargar_items_a_la_linea();
+
+
 
 
         //se le agrega el evento on click para capturar las pulsaciones
+        TimeLine.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(time_line.this, "pulsaste " + i, Toast.LENGTH_SHORT).show();
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this,drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        globales.manager=getSupportFragmentManager();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
 
-        swipeRefreshLayout=(SwipeRefreshLayout) findViewById(R.id.swipe);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Boolean estado=onNavigationItemSelected(null);
-                if (estado){
-                    swipeRefreshLayout.setRefreshing(false);
-                }
-            }
-        });
-        menu = (NavigationView) findViewById(R.id.nav_view);
-        menu.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        int ids = menuItem.getItemId();
-                        id=menuItem.getItemId();
-                        if (ids == R.id.nav_timeLine) {
-
-                            Toast.makeText(getApplicationContext(), " time line", Toast.LENGTH_SHORT).show();
-                        } else if (ids == R.id.nav_gallery) {
-
-                            Toast.makeText(getApplicationContext(), " galeria", Toast.LENGTH_SHORT).show();
-                        } else if (ids == R.id.nav_slideshow) {
-
-                            Toast.makeText(getApplicationContext(), " sliders", Toast.LENGTH_SHORT).show();
-                        } else if (ids == R.id.nav_login) {
-
-                        } else if (ids == R.id.nav_share) {
-
-                        } else if (ids == R.id.nav_send) {
-
-                        }
-
-                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                        drawer.closeDrawer(GravityCompat.START);
-                        return true;
-                    }
-                }
-        );
-        onNavigationItemSelected(null);
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -109,47 +116,65 @@ public class time_line extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        boolean estado=false;
-        //id=item.getItemId();
-        Fragment fragment_actual=null;
+        int id = item.getItemId();
+
         if (id == R.id.nav_timeLine) {
-            fragment_actual=new fragment_timeLine();
-            estado=true;
-            Toast.makeText(this, " time line " +id, Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_gallery) {
-            Toast.makeText(this, " galeria "+id, Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_slideshow) {
-            Toast.makeText(this, " sliders "+id, Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.nav_login) {
+
+        } else if (id == R.id.menu_sobreUMG) {
+            Intent i=new Intent(this, info_u.class);
+            startActivity(i);
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
-        if (estado){
-            getFragmentManager().beginTransaction().replace(R.id.contenedor,fragment_actual).commit();
-        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
     public void evento(View view) {
         Intent llamada = new Intent(time_line.this, agregar_item_line.class);
         startActivity(llamada);
         Snackbar.make(view, "trabajamos en esta opcion!! gracias! ", Snackbar.LENGTH_LONG).show();
     }
+
     public void noticia(View v) {
+        db_timeLine time = new db_timeLine(this, 1);
+        SQLiteDatabase db;
+        db = time.getWritableDatabase();
         Snackbar.make(v,"felicidades sin querer as creado una base de datos de 5000000 campos ",Snackbar.LENGTH_LONG).show();
+    }
+
+    ///------ metodo que se ejecuta para cargar los items que se muestran en la linea de tiempo
+    private void cargar_items_a_la_linea() {
+
+        adaptadorContentTimeLine = new AdaptadorContentTimeLine(this);
+
+        TimeLine.setAdapter(adaptadorContentTimeLine);
+
     }
 }
